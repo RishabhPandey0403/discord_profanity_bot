b3b1675a10183251b735673e1d4762b652cf047f
Rishabh Pandey <email.rishabhp@gmail.com>
2021-10-09 19:27:29 -0400

    added an embed, tried to hide token using an interface

--- src/main/java/ProfanityBot.java
+++ src/main/java/ProfanityBot.java
@@ -1,17 +1,28 @@
+import commands.Token;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 import reactor.core.publisher.Mono;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProfanityBot {
     public static void main(String[] args) {
-        DiscordClient client = DiscordClient.create("ODk2MjA2MjYzMzkyODE3MTgz.YWDvHw.DQmkoU2oqI0BBXWpiFsVwV1s0_8");
+        DiscordClient client = DiscordClient.create(Token.token).build().login().block()
 
         Mono<Void> stopWarning = client.withGateway((GatewayDiscordClient gateway) -> {
 
@@ -26,17 +37,29 @@ public class ProfanityBot {
                         list.add(myReader.nextLine());
 
                     if (list.contains(message.getContent().toLowerCase())) {
-                        return message.getChannel().flatMap(channel -> channel.createMessage("stop!"));
+                        return message.getChannel().
+                                flatMap(channel -> channel.createMessage("stop!"));
                     }
                 } catch (FileNotFoundException e) {
                     System.out.println("An error occurred.");
                     e.printStackTrace();
                 }
                 return Mono.empty();
-            }).then();
+            })
+        };
 
-            //gateway.on()
-            //return todo
-        });
+        Color rgbColor = Color.of(255, 0, 127);
+
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(rgbColor)
+                .title("Language Warning!")
+                .description("This is a warning to avoid using profane language")
+                .image("https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FKcj%2Fo9R%2FKcjo9RjBi.png&f=1&nofb=1")
+                .timestamp(Instant.now())
+                .author("Server owner", null, null)
+                .build();
+        //gateway.on()
+        //return todo
+        );
     }
 }
---
+++ src/main/java/commands/Token.java
@@ -0,0 +1,5 @@
+package commands;
+
+public interface Token {
+    String token = "ODk2MjA2MjYzMzkyODE3MTgz.YWDvHw.DQmkoU2oqI0BBXWpiFsVwV1s0_8";
+}
